6fddfd95d7e5f7ade1a58d893cacc825a14523df
stefan judis <stefanjudis@gmail.com>
2017-07-22 15:00:03 +0200

    rename gif

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ async function init() {
   try {
     // const options = await inquirer.prompt( config.questions );
     const client = cli.createClient ( {
-      spaceId     : '0pnbjplyokup',
-      accessToken : 'CFPAT-fee352c01532d2f3f3092fb11e3c79c01515bd1dbc858c40a81b666f7f903ede'
+      spaceId     : '',
+      accessToken : ''
     } );
 
     const savedExtension = await client.save( {
